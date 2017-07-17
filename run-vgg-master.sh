d423c847dec3191cbda4e49ff1826638a7972561
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2017-07-17 00:20:55 +0000

    Add script used to run vgg

---
+++ run-vgg-master.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source scripts/bigdl.sh
+
+/home/ubuntu/drizzle-spark/bin/spark-submit \
+--verbose \
+--master spark://ip-10-0-0-98.us-west-2.compute.internal:7077 \
+--driver-memory 20g \
+--executor-memory 20g \
+--executor-cores 4 \
+--total-executor-cores 128 \
+--driver-class-path /home/ubuntu/BigDL/spark/dl/target/bigdl-0.2.0-SNAPSHOT-jar-with-dependencies.jar \
+--class com.intel.analytics.bigdl.models.vgg.Train \
+/home/ubuntu/BigDL/spark/dl/target/bigdl-0.2.0-SNAPSHOT-jar-with-dependencies.jar \
+-f /mnt/ebs-vol/cifar-10-batches/cifar-10-batches-bin \
+--batchSize 896 \
+--checkpoint /mnt/ebs-vol/vgg-model \
+--maxEpoch 2
